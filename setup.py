2f564e990bc7645a6afa97c4bc010665a6229f57
Jon Tutcher <jon.tutcher@bbc.co.uk>
2015-06-24 11:12:29 +0100

    Initial commit---scaffolding

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(name='cleverthumbnailer',
+      version='0.1.0',
+      description='Selects interesting and relevant short thumbnails for longer audio tracks',
+      author='Jon Tutcher',
+      author_email='jon.tutcher@bbc.co.uk',
+      packages = ['cleverthumbnailer'],
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+
+      ]
+      )
