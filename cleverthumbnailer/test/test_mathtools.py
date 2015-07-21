a2dc4ba8c1981ae4aa2e03f6f18414e2e3b41361
Jon Tutcher <jon.tutcher@bbc.co.uk>
2015-07-21 11:23:42 +0100

    Add interpolate tests
    Added three new tests to `test_mathtools.py` to test for the following:
    - Irregular length input tuples (should raise ValueError)
    - Bad datatype inputs (should raise ValueError)
    - Passing inputs as lists rather than tuples (should work)
    
    All new (and old) tests pass.

--- cleverthumbnailer/test/test_mathtools.py
+++ cleverthumbnailer/test/test_mathtools.py
@@ -5,6 +5,7 @@ from cleverthumbnailer.mathtools import interpolate
 from unittest import TestCase, main
 
 class TestInterpolate(TestCase):
+    """Test `interpolate()` function in `mathtools.py`"""
 
     def test_flatLine(self):
         XY1 = (0, 1)
@@ -40,6 +41,32 @@ class TestInterpolate(TestCase):
         XY1 = (1, 0)
         XY2 = (0, 1)  # X1 > X2, values increasing between 1 and 2 (so effectively decreasing)
         results = ((0, 1), (0.5, 0.5), (0.8, 0.2), (1, 0))
+        self.checkInterpolate(XY1, XY2, results)
+
+    def test_extrapolate(self):
+        XY1 = (5, 15)   # set up y = (2x)+5
+        XY2 = (10, 25)
+        results = ((2.5, 10), (0, 5), (7.5, 20), (30, 65))
+        self.checkInterpolate(XY1, XY2, results)
+
+    def test_invalidInputTuples(self):
+        XY1 = (1, 1, 1)     # should raise ValueError
+        XY2 = (1, 1)
+        results = ((1, None))
+        with self.assertRaises(ValueError):
+            self.checkInterpolate(XY1, XY2, results)
+
+    def test_invalidInter(self):
+        XY1 = (1, 1)
+        XY2 = (1, 1)
+        results = (('string', None))    # should not accept string inputs
+        with self.assertRaises(ValueError):
+            self.checkInterpolate(XY1, XY2, results)
+
+    def test_inputList(self):
+        XY1 = [0, 1]        # send inputs as lists not as tuples
+        XY2 = [1, 2]        # set up y = (1)x + 1
+        results = ((0, 1), (0.3, 1.3), (1, 2))
         self.checkInterpolate(XY1, XY2, results)
 
     def checkInterpolate(self, XY1, XY2, results):
