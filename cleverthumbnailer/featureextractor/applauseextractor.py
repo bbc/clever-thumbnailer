70088d264f5c938038fc4f23017aaf3f722fb93c
Jon Tutcher <jon.tutcher@bbc.co.uk>
2015-08-03 14:44:54 +0100

    Re-factored GenericExtractor for FFT
    Subclassed `GenericExtractor()` into `FrequencyDomainExtractor` and
    `TimeDomainExtractor`:
    	- processFrame() behaves as before
    	- Each subclass now returns the correct enum on the `frameDomain`
    	property
    	- A new `processTimeDomainFrame()` method behaves differently for
    	each subclass. Calling this on a time domain plugin passes
    	arguments directly thgouh to `processFrame()`. Calling it on a
    	frequency domain plugin calls numpy.fft to transform time domain
    	blocks into frequency domain before processing with
    	`processFrame`.
    	- A new `processAllAudio()` method processes an entire song using
    	the plugin. This behaves correctly for both time and frequency
    	domain plugins.

--- cleverthumbnailer/featureextractor/applauseextractor.py
+++ cleverthumbnailer/featureextractor/applauseextractor.py
@@ -1,12 +1,92 @@
 #!/usr/bin/env python
 __author__ = 'Jon'
 
+# Default Step size: 512
+# Default Block size: 1024 (for 48khz)
+
 import febase
 import numpy
+import enum
+import logging
+from collections import deque
+from freqdomainextractor import FrequencyDomainExtractor
 
+class ApplauseState(enum.Enum):
+    applause = 0
+    music = 1
 
-class ApplauseExtractor(febase.GenericExtractor):
-    def __init__(self, sr, blockSize=1024):
+class ApplauseExtractor(FrequencyDomainExtractor):
+    def __init__(self, sr, blockSize=1024, stepSize=512, threshold=0.04, hysteresis=0.02):
         super(ApplauseExtractor, sr).__init__()
+        self._logger = logging.getLogger(__name__)
         self._blockSize = blockSize
+        self._stepSize = stepSize
         self._features = numpy.array([])
+        self.threshold = threshold
+        self.hysteresis = hysteresis
+        self._currentApplauseState = None
+        self.movingAverageBuffer = None
+
+
+    def resizeMovAvgBuffer(self):
+        self.movingAverageBuffer = deque([], maxlen=self.moving_average)
+        return
+
+    def calculateBufferMean(self):
+        return numpy.mean(self.movingAverageBuffer)
+
+    @property
+    def blockSize(self):
+        return self._blockSize
+
+    @property
+    def stepSize(self):
+        return self._stepSize
+
+    def processFrame(self, frame, timestamp):
+        """Process one frame of signal according to frequency domain block input.
+
+        Args:
+            frame(Complex[]): frequency domain (FFT) frame. Should be of length .blockSize
+            timestamp(int): starting sample of block
+        Returns:
+            crest:
+        """
+        assert len(frame) >= self.blockSize
+        spectralCrestFactor = spectralCrest(frame)  # obtain spectral crest factor for processed window
+        self.movingAverageBuffer.append(spectralCrestFactor)    # add SCF to buffer
+        bufferMean = self.calculateBufferMean()
+        newState = self.applauseDetection(bufferMean)   # see if we have a new feature (speech/music) for current buffer
+        if newState:
+            self.features.append((newState, timestamp)) # record state and time of new feature
+            self._logger.debug('New Event: {0} at time {1}'.format(newState, timestamp))
+
+    def processRemaining(self):
+        pass
+
+    def applauseDetection(self, frame):
+        threshold = self.threshold
+        hysteresis = self.hysteresis
+        lthresh = threshold-(hysteresis/2)
+        uthresh = threshold+(hysteresis/2)
+
+        if self._currentApplauseState is ApplauseState.music:
+            if frame < lthresh:
+                self._currentApplauseState = ApplauseState.applause
+                return self._currentApplauseState
+        else:
+            if frame > uthresh:
+                self._currentApplauseState = ApplauseState.music
+                return self._currentApplauseState
+        return None
+
+def spectralCrest(self, freqDomainFrame):
+    """Calculate the crest factory of a block of audio
+    Args:
+        freqDomainFrame(complex array-like): frequency domain information for audio block
+    Returns:
+        crestFactor(int): spectral crest factor of audio
+    """
+    magnitudeSpectrum = numpy.absolute(freqDomainFrame) / (len(freqDomainFrame)*0.5)
+    totalSpectralPower = magnitudeSpectrum.sum()
+    return magnitudeSpectrum.max() / totalSpectralPower    # crest
---
+++ cleverthumbnailer/test/test_applauseExtractor.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+__author__ = 'jont'
+
+
+class TestApplauseExtractor(TestCase):
+  def test_resizeMovAvgBuffer(self):
+    self.fail()
+
+  def test_calculateBufferMean(self):
+    self.fail()
+
+  def test_blockSize(self):
+    self.fail()
+
+  def test_stepSize(self):
+    self.fail()
+
+  def test_processFrame(self):
+    self.fail()
+
+  def test_processRemaining(self):
+    self.fail()
+
+  def test_applauseDetection(self):
+    self.fail()
