88755dc0fb45944ed07c9fdbe88ee814fc83f1d4
Jon Tutcher <jon@jtutcher.co.uk>
2015-08-06 09:39:15 +0100

    Refactor `setup.py`
    - Switch from distutils to setuptools

--- cleverthumbnailer/audiodata.py
+++ cleverthumbnailer/audiodata.py
@@ -1,15 +1,23 @@
 import numpy
 import wave
+from exceptions import FileFormatNotSupportedError
 
 class AudioData(object):
+    """Container for low level mono audio information and IO.
+
+    Attributes:
+        waveData (list): mono-averaged audio samples from track
+        sr (int): audio sample rate
+        bitdepth (int): audio bit depth
+    """
+
     def __init__(self, filename):
         self._wavefile = None
-        self._bytesPerSample = None
         self._sampleRate = None
         self._length = None
         self._waveData = None
-        self.comptype = None
         self.loaded = False
+        self.bitDepth = None
         self.loadFile(filename)
         pass
 
@@ -26,33 +34,51 @@ class AudioData(object):
         raise ValueError('No file loaded')
 
     def loadFile(self, filename):
+        """Load audio file into AudioData object]
+
+        Args:
+            filename (str): Audio filename for loading
+
+        Raises:
+            FileFormatNotSupportedError: if the audio file loaded is not a
+            16 or 24-bit WAVE file
+        """
         self._wavefile = filename
         w = wave.open(filename, 'r')
-        (nchannels, self._bytesPerSample, self._sampleRate, self._length, comptype, compname) = w.getparams ()
-        if self._bytesPerSample==1:
+        (nchannels, self.bitDepth, self._sampleRate, self._length,
+         _, _) = w.getparams ()
+        if self.bitDepth==1:
             origType = numpy.int8
-        elif self._bytesPerSample==2:
+        elif self.bitDepth==2:
             origType = numpy.int16
-        else: raise ValueError('{0}-bit sample depth not supported'.format(self._bytesPerSample*8))
+        else: raise FileFormatNotSupportedError('{0}-bit sample depth not supported'.format(
+            self.bitDepth*8))
         wavType = numpy.dtype((origType,nchannels))
         self.npOut = numpy.frombuffer(w.readframes(self._length), dtype=wavType)
         self._waveData = numpy.mean(self.npOut,1) / numpy.iinfo(origType).max
         self.loaded = True
 
-    def frames(self, step_size, frame_size):
+    def frames(self, stepSize, frameSize):
+        """Generate sequential window based on known step and block size
+
+        Args:
+            stepSize (int): size of step between frames
+            frameSize (int): size of window (or block) to be returned with
+            each iteration
+
+        Yields:
+            list: audio frame of size frameSize
         """
-        Take step and frame size, and return iterator over audio data for processing in blocks.
-        """
-        assert(step_size > 0)
+        assert(stepSize > 0)
         assert(self.waveData.ndim == 1)
         n = self.waveData.shape[0]
         print(n)
         i = 0
         while i < n:
-            frame = self.waveData[i : i + frame_size]
+            frame = self.waveData[i : i + frameSize]
             w = frame.shape[0]
-            if w < frame_size:
-                pad = numpy.zeros((frame_size - w))
+            if w < frameSize:
+                pad = numpy.zeros((frameSize - w))
                 frame = numpy.concatenate((frame, pad), 1)
             yield frame.tolist()
-            i = i + step_size
+            i = i + stepSize
--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
-from distutils.core import setup
-setup(name='cleverthumbnailer',
-      version='0.1.0',
-      description='Selects interesting and relevant short thumbnails for longer audio tracks',
-      author='Jon Tutcher',
-      author_email='jon.tutcher@bbc.co.uk',
-      packages = ['cleverthumbnailer'],
-      entry_points={
-          'console_scripts': [
-              'cleverthumbnailer =  cleverthumbnailer.__main__:main'
-          ]
-      }, requires=['numpy']
-      classifiers=[
-          'Development Status :: 1 - Planning',
-          'Environment :: Console',
-          'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+from setuptools import setup
 
-      ]
-      )
+setup(
+    name='cleverthumbnailer',
+    version='0.1.0',
+    description='Selects interesting and relevant short thumbnails for '
+                'longer audio tracks',
+    author='Jon Tutcher',
+    author_email='jon.tutcher@bbc.co.uk',
+    packages=['cleverthumbnailer'],
+    entry_points={
+        'console_scripts': [
+            'cleverthumbnailer =  cleverthumbnailer.__main__:main'
+        ]
+    },
+    install_requires=[
+        'numpy',
+        'qmsegmenter',
+        'pysox'
+
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v2 or ' +
+        'later (GPLv2+)'
+    ]
+)
