c029f06318c9b96af0561b71efb9feee2385d0f7
Nicholas Humfrey <njh@aelius.com>
2016-08-16 11:52:38 +0100

    Setup automake based build system

---
+++ src/clever-thumbnailer.c
@@ -0,0 +1,25 @@
+/*
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include <getopt.h>
+
+#include "clever-thumbnailer.h"
+
+
+
+int main(int argc, char *argv[])
+{
+
+  printf("Hello clever-thumbnailer.\n");
+
+  // Success
+  return 0;
+}
---
+++ src/clever-thumbnailer.h
@@ -0,0 +1,19 @@
+/*
+
+*/
+
+#include "config.h"
+
+#include <stdarg.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+
+#ifndef CLEVER_THUMBNAILER_H
+#define CLEVER_THUMBNAILER_H
+
+
+
+
+#endif
